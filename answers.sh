2c9969f980b5557dea648657456adce0d0c8a4ea
Dana Allam <dana.allam99@hotmail.com>
2021-06-03 23:00:17 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -6,3 +6,14 @@ Basic queries
 5) insert into students(Name, Age, Gender, Points) values('Dana', '21', 'F', '750');
 6) update students set Points=Points+5 where name='Basma';
 7) update students set Points=Points-5 where name='Alex';
+8) CREATE TABLE graduates(
+	ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	Name Text NOT NULL UNIQUE,
+	Age INTEGER,
+	Gender TEXT,
+	Points INTEGER,
+	Graduation TEXT
+   );
+9) INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM students WHERE name="Layal";
+10) UPDATE graduates set Graduation='08/09/2018' WHERE name='Layal';
+11) DELETE FROM students WHERE name='Layal';
