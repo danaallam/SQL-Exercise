884a536470002e804b2fd92b701195396e65a63d
Dana Allam <dana.allam99@hotmail.com>
2021-06-03 23:59:55 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -8,7 +8,7 @@ Basic queries
 7) UPDATE students SET Points=Points-5 WHERE name='Alex';
 
 Creating Table
-8) CREATE TABLE graduates(
+1) CREATE TABLE graduates(
 	ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	Name Text NOT NULL UNIQUE,
 	Age INTEGER,
@@ -16,11 +16,18 @@ Creating Table
 	Points INTEGER,
 	Graduation TEXT
    );
-9) INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM students WHERE name="Layal";
-10) UPDATE graduates set Graduation='08/09/2018' WHERE name='Layal';
-11) DELETE FROM students WHERE name='Layal';
+2) INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM students WHERE name="Layal";
+3) UPDATE graduates set Graduation='08/09/2018' WHERE name='Layal';
+4) DELETE FROM students WHERE name='Layal';
 
 Joins
-12) SELECT e.name, c.name as Company, c.date from employees e INNER JOIN companies c where e.Company=c.name;
-13) SELECT name from (SELECT e.name, c.name, c.date from employees e INNER JOIN companies c where e.Company=c.name) where date<=2000;
-14) SELECT comp from (SELECT e.name, c.name as comp, c.date from employees e INNER JOIN companies c where e.Company=c.name), employees e where comp=e.Company AND e.Role='Graphic Designer';
+5) SELECT e.name, c.name as Company, c.date from employees e INNER JOIN companies c where e.Company=c.name;
+6) SELECT name from (SELECT e.name, c.name, c.date from employees e INNER JOIN companies c where e.Company=c.name) where date<=2000;
+7) SELECT comp from (SELECT e.name, c.name as comp, c.date from employees e INNER JOIN companies c where e.Company=c.name), employees e where comp=e.Company AND e.Role='Graphic Designer';
+
+Count & Filter
+1) SELECT name, Points FROM students WHERE Points=(SELECT max(Points) FROM students);
+2) SELECT avg(Points) FROM students;
+3) SELECT count() as studentsAbove500 FROM students WHERE Points=500;
+4) SELECT name FROM students WHERE name like '%s%';
+5) SELECT * FROM students ORDER BY Points DESC;
