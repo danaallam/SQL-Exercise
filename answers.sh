8b55cda2283153351439e8a69e1218f213b58f4b
Dana Allam <dana.allam99@hotmail.com>
2021-06-03 22:32:44 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,8 @@
+Basic queries
+1) select name from students;
+2) select * from students where Age>=30;
+3) select name from students where Gender="F" and Age>=30;
+4) select points from students where name="Alex";
+5) insert into students(Name, Age, Gender, Points) values('Dana', '21', 'F', '750');
+6) update students set Points=Points+5 where name='Basma';
+7) update students set Points=Points-5 where name='Alex';
